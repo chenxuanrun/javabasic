bdfc93099640cb5b6f869ff7436824037074b3f5
陈宣润 <815135619@qq.com>
2019-09-16 16:47:02 +0800

    inti

---
+++ src/socket/TCPClient.java
@@ -0,0 +1,10 @@
+package socket;
+
+/**
+ * @program: javabasic
+ * @description: TCP客户端
+ * @author: cxr
+ * @create: 2019-09-16 16:43
+ **/
+public class TCPClient {
+}
---
+++ src/socket/TCPServer.java
@@ -0,0 +1,13 @@
+package socket;
+
+/**
+ * @program: javabasic
+ * @description: TCP服务端
+ * @author: cxr
+ * @create: 2019-09-16 16:43
+ **/
+public class TCPServer {
+    public static void main(String[] args) {
+        
+    }
+}
